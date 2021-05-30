6723b5ec66093fece855528a3ce65c50b0f55109
along <wuhl6@mail2.sysu.edu.cn>
2021-05-30 13:11:01 +0800

    install vim plugin when running install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# install tools needed by vim plugins
 sudo apt install silversearcher-ag
 sudo apt install clang-format
+
+# install Vundle to manage vim plugins
+vundle_dir="$HOME/.vim_runtime/my_plugins/Vundle.vim"
+if [ ! -d "$vundle_dir" ]; then
+    mkdir -p $vundle_dir
+    git clone https://github.com/VundleVim/Vundle.vim.git $vundle_dir
+fi
+
+# install vim plugins
+vim -E +PluginInstall +qall
+
